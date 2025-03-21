c1366a576db788c524cb1028dd9d4ee2ea401484
GitHub Action <action@github.com>
2025-03-21 07:14:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/jinho622/capstone2intellijplugindevlog/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/jinho622/capstone2intellijplugindevlog/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jinho622.capstone2intellijplugindevlog.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/jinho622/capstone2intellijplugindevlog/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jinho622/capstone2intellijplugindevlog/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jinho622.capstone2intellijplugindevlog.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jinho622.capstone2intellijplugindevlog.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
